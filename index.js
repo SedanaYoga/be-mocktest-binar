9df37912baa7a44e6b93c9a71f403528dc75c30e
Sedana Yoga <9g.yoga27@gmail.com>
2022-07-23 17:08:53 +0800

    add: cors and Swagger UI update

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const routes = require('./routes')
 const passportJwt = require('./config/passport')
 const cors = require('cors')
 
-// Swagger UI
-const swaggerUi = require('swagger-ui-express')
-const swaggerDoc = require('./config/swaggerBinar.json')
-
 // App Initialization
 const app = express()
 
@@ -16,17 +12,17 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(require('morgan')('dev'))
 }
 
-// const corsOptions = {
-//   origin: '*',
-//   methods: ['GET', 'POST'],
-//   credentials: true,
-// }
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST'],
+  credentials: true,
+}
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(passportJwt.initialize())
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 // Routes
 app.use(routes)
